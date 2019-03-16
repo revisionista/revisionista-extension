185690b4540b13b9aee3cfb1c127bddd2049fea3
Flavio Martins <flaviomartins@acm.org>
2019-03-16 23:05:08 +0000

    add tufte-css to reader view

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -4,6 +4,26 @@ var article = new Readability(documentClone).parse();
 console.log(article);
 console.log(`Readability end`)
 
+// Template for reader view.
+var head_template = `
+  <meta charset="utf-8"/>
+  <title>${article.title}</title>
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/tufte-css/1.4/tufte.min.css"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+`
+
+var body_template = `
+  <article id="readability-container">
+    <h1>${article.title}</h1>
+    <p>${article.content}</p>
+  </article>
+`
+
+// Try to replace everything.
+document.body.outerHTML = '';
+document.head.outerHTML = '';
+document.head.innerHTML = head_template;
+document.body.innerHTML = body_template;
 
 function handleResponse(message) {
   console.log(`Message from the background script:  ${message.response}`);
