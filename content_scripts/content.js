7c4bfdd4e6ec7b8f688a7a117aee838253cfe267
Flavio Martins <flaviomartins@acm.org>
2019-03-26 21:39:42 +0000

    use charMode for titles and wordMode for content

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,6 +1,40 @@
 var documentClone = document.cloneNode(true);
 var article = new Readability(documentClone).parse();
+
 var dmp = new diff_match_patch();
+String.prototype.regexIndexOf = function(regex, startpos) {
+  var indexOf = this.substring(startpos || 0).search(regex);
+  return (indexOf >= 0) ? (indexOf + (startpos || 0)) : indexOf;
+}
+diff_match_patch.prototype.diff_linesToWords_=function(a,b){function c(a){for(var b="",c=0,g=-1,h=d.length;g<a.length-1;){g=a.regexIndexOf("\\W",c);-1==g&&(g=a.length-1);var l=a.substring(c,g+1);(e.hasOwnProperty?e.hasOwnProperty(l):void 0!==e[l])?b+=String.fromCharCode(e[l]):(h==f&&(l=a.substring(c),g=a.length),b+=String.fromCharCode(h),e[l]=h,d[h++]=l);c=g+1}return b}var d=[],e={};d[0]="";var f=4E4,g=c(a);f=65535;var h=c(b);return{chars1:g,chars2:h,lineArray:d}};
+
+function diff_lineMode(text1, text2) {
+  var a = dmp.diff_linesToChars_(text1, text2);
+  var lineText1 = a.chars1;
+  var lineText2 = a.chars2;
+  var lineArray = a.lineArray;
+  var diffs = dmp.diff_main(lineText1, lineText2, true);
+  dmp.diff_cleanupSemantic(diffs);
+  dmp.diff_charsToLines_(diffs, lineArray);
+  return diffs;
+}
+
+function diff_wordMode(text1, text2) {
+  var a = dmp.diff_linesToWords_(text1, text2);
+  var lineText1 = a.chars1;
+  var lineText2 = a.chars2;
+  var lineArray = a.lineArray;
+  var diffs = dmp.diff_main(lineText1, lineText2, true);
+  dmp.diff_cleanupSemantic(diffs);
+  dmp.diff_charsToLines_(diffs, lineArray);
+  return diffs;
+}
+
+function diff_charMode(text1, text2) {
+  var diffs = dmp.diff_main(text1, text2);
+  dmp.diff_cleanupSemantic(diffs);
+  return diffs;
+}
 
 // Template for reader view.
 var head_template = `
@@ -31,9 +65,7 @@ function diff_title() {
   var text1 = document.getElementById('readability-1-title').textContent.trim();
   var text2 = document.getElementById('readability-2-title').textContent.trim();
 
-  var d = dmp.diff_main(text2, text1);
-  dmp.diff_cleanupSemantic(d);
-  // dmp.diff_cleanupEfficiency(d);
+  var d = diff_charMode(text2, text1);
   var ds = dmp.diff_prettyHtml(d);
   document.getElementById('readability-1-title').innerHTML = ds;
   document.getElementById('readability-2-title').innerHTML = '';
@@ -69,9 +101,7 @@ function diff_content() {
   text1 = text1.textContent.replace(/\n\s*\n\s*\n/g, '\n\n').trim();
   text2 = text2.textContent.replace(/\n\s*\n\s*\n/g, '\n\n').trim();
 
-  var d = dmp.diff_main(text2, text1);
-  dmp.diff_cleanupSemantic(d);
-  // dmp.diff_cleanupEfficiency(d);
+  var d = diff_wordMode(text2, text1);
   var ds = dmp.diff_prettyHtml(d);
   document.getElementById('readability-1-content').innerHTML = '<p>' + ds + '</p>';
   document.getElementById('readability-2-content').innerHTML = '';
